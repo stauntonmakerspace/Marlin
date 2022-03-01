3dd6704038f41499e7e76a8a61c552fb1623a24a
aroronchynn <aroronchynn@gmail.com>
2022-02-28 20:07:05 -0500

    Update definition to CR-10S

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,7 +143,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3 Max"
+#define CUSTOM_MACHINE_NAME "Creality CR-10S"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1387,7 +1387,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 300
+#define Z_MAX_POS 400
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
